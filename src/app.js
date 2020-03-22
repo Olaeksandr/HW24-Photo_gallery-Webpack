051de3a4c08f031ef08cac873871fd64b35caa47
Oleksandr Masiuk <ommasiuk@gmail.com>
2020-03-22 19:06:14 +0200

    your plagin is work

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import $ from 'jquery';
 import API from './api';
-require('imports-loader?$=jquery!../node_modules/imagelightbox/dist/imagelightbox.min.js');
-import '../node_modules/imagelightbox/dist/imagelightbox.min.css';
+// require('imports-loader?$=jquery!../node_modules/imagelightbox/dist/imagelightbox.min.js');
+// import '../node_modules/imagelightbox/dist/imagelightbox.min.css';
+
+require('imports-loader?jQuery=jquery!../node_modules/simplelightbox/dist/simple-lightbox.jquery.js');
+import '../node_modules/simplelightbox/dist/simple-lightbox.css';
 
 $(() => {
     const $imageList = $('#imageList');
@@ -21,13 +24,16 @@ $(() => {
     function generateImageHTML(image) {
         console.log('Image');
         return $imageTemplate
-        .replace('{{id}}', image.id)
+        // .replace('{{id}}', image.id)
         .replace('{{title}}', image.title)
         .replace('{{url}}', image.url)
         .replace('{{thumbnailUrl}}', image.thumbnailUrl);
     }
     function initGallary() {
-        $('a[data-imagelightbox="demo"]').imageLightbox();
+        // $('a[data-imagelightbox="demo"]').imageLightbox();
+        $imageList.find('a').simpleLightbox({
+            fileExt: false
+        });
     }
 });
 
